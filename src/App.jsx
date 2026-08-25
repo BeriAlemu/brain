198342dd8e1ed4488a39b929b68b554c5c3220b5
Beri Alemu <berialemu8@gmail.com>
2026-08-25 23:56:22 +0300

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
 
+
+
 function App() {
   return ( 
     <>
-        <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden button">
           <Header />
         </div>
     </>
